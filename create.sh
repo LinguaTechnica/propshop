de0405729ba1cb360880a59cdc326f8e9e2ea28c
Perry A Branch <perry.branch@galvanize.com>
2019-05-24 10:33:30 -0600

    remove create script; use aws cloudformation instead

--- create.sh
+++
@@ -1,3 +0,0 @@
-aws s3api create-bucket --bucket rentalated --region us-east-1
-aws s3 website s3://rentalated --index-document index.html --error-document error.html
-aws s3api put-bucket-policy --bucket rentalated --policy file://bucket-policy.json
