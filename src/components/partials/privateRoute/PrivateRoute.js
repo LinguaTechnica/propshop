b1cbd954c32b67f27dd1db8c824af2e9011a0acb
Kay Hudson <kay.hudson@galvanize.com>
2019-06-11 12:47:38 -0700

    Refactor AuthService and PrivateRoute
    Effort to centralize all auth. Main app can pass auth status thru to
    other components for authorization. PrivateRoute should be able to do
    a cursory check and allow the component access to auth status live.

--- src/components/partials/privateRoute/PrivateRoute.js
+++ src/components/partials/privateRoute/PrivateRoute.js
@@ -1,61 +1,68 @@
 import React from 'react'
 import { Redirect, Route } from 'react-router-dom'
 
-import { isLoggedIn } from '../../../services/authService'
+// import { isLoggedIn } from '../../../services/authService'
 
-class PrivateRoute extends React.Component {
+// TODO: do willMount token verification in the passed-in components?
+const PrivateRoute = ({ component: Component, isAuthorized}) => {
+  return (
+      <Route render={(props) => isAuthorized() ? <Component {...props} /> : <Redirect to={{pathname: '/login'}}/>} />
+  )
+};
 
-  // make verification request
-  // render a loading gif
-  // redirect to login or render component
-  state = {
-    loggedIn: null,
-    tokenVerified: false
-  }
+// class PrivateRoute extends React.Component {
+//
+//   // make verification request
+//   // render a loading gif
+//   // redirect to login or render component
+//   state = {
+//     loggedIn: null,
+//     tokenVerified: false
+//   }
+//
+//   componentWillMount() {
+//     isLoggedIn()
+//       .then(
+//         () => {
+//           this.setState({
+//             loggedIn: true,
+//             tokenVerified: true
+//           })
+//         },
+//         () => {
+//           console.log('caught rejected promise')
+//           this.setState({
+//             loggedIn: false,
+//             tokenVerified: true
+//           })
+//         }
+//       )
+//   }
+//
+//   render() {
+//     const { component: Component, ...rest } = this.props
+//     let toRender = <img src="loader.gif" alt="loading gif" />
+//     if (this.state.tokenVerified) {
+//       toRender = this.state.loggedIn ? (
+//         <Component {...rest} />
+//       ) : (
+//         <Redirect to={{ pathname: '/login' }} />
+//       )
+//         // <Route
+//         //   {...rest}
+//         //   render={props => {
+//         //     console.log('rendering from renderprops')
+//         //       return this.state.loggedIn ? (
+//         //         <Component {...props} />
+//         //       ) : (
+//         //         <Redirect to={{ pathname: '/login', state: { referrer: props.location } }} />
+//         //       )
+//         //     }
+//         //   }
+//         // />
+//     }
+//     return toRender
+//   }
+// }
 
-  componentWillMount() {
-    isLoggedIn()
-      .then(
-        () => {
-          this.setState({
-            loggedIn: true,
-            tokenVerified: true
-          })
-        },
-        () => {
-          console.log('caught rejected promise')
-          this.setState({
-            loggedIn: false,
-            tokenVerified: true
-          })
-        }
-      )
-  }
-
-  render() {
-    const { component: Component, ...rest } = this.props
-    let toRender = <img src="loader.gif" alt="loading gif" />
-    if (this.state.tokenVerified) {
-      toRender = this.state.loggedIn ? (
-        <Component {...rest} />
-      ) : (
-        <Redirect to={{ pathname: '/login' }} />
-      )
-        // <Route
-        //   {...rest}
-        //   render={props => {
-        //     console.log('rendering from renderprops')
-        //       return this.state.loggedIn ? (
-        //         <Component {...props} />
-        //       ) : (
-        //         <Redirect to={{ pathname: '/login', state: { referrer: props.location } }} />
-        //       )
-        //     }
-        //   }
-        // />
-    }
-    return toRender
-  }
-}
-
-export default PrivateRoute
+export default PrivateRoute;
--- src/services/authService.js
+++ src/services/authService.js
@@ -2,38 +2,55 @@ import request from 'superagent'
 
 import { accountsEndpoint, tokenEndpoint } from "../config"
 
-export const isLoggedIn = () => new Promise((res,rej) => {
-    console.log("CHECKING SESSION TOKEN");
-    const sessionToken = localStorage.getItem("session_token");
-    if (!sessionToken) {
-        rej()
+class AuthService {
+    constructor() {
+        this.isAuthenticated = false;
+        this.isAuthorized = false;
+        this.sessionToken = localStorage.getItem('session_token');
     }
-    else {
-        request.post(tokenEndpoint)
+
+    isLoggedIn() {
+        if (!this.sessionToken) return false;
+
+        // TODO: how do we want to use this? returns object with token? token? boolean?
+        return new Promise((res, rej) => {
+            console.log("CHECKING SESSION TOKEN");
+                request.post(tokenEndpoint)
+                    .set("Content-Type", "application/json")
+                    .send({ auth_header: this.sessionToken })
+                    .then(resp => {
+                            console.log("success response from token check", resp);
+                            if (resp.ok) {
+                                this.isAuthorized = true;
+                                return res(resp)
+                            } else {
+                                return rej(resp)
+                            }
+                        },
+                        err => {
+                            console.log("error response from token check", err);
+                            return rej(err)
+                        })
+        })
+    }
+
+    logIn(userData) {
+        return request.post(accountsEndpoint)
             .set("Content-Type", "application/json")
-            .send({
-                auth_header: sessionToken
+            .send(userData)
+            .then((res) => {
+                console.log('Successful login: ', res.ok);
+                this.isAuthenticated = true;
+                // TODO: How many kinds of user auth needed?
+                // this.isAuthorized = true;
             })
-            .then(resp => {
-                console.log("success response from token check", resp);
-                if (resp.valid) {
-                    return res(resp)
-                }
-                else {
-                    return rej(resp)
-                }
-            },
-            err => {
-                console.log("error response from token check", err);
-                return rej(err)
-            })
+            .catch(err => console.error(err))
+    };
+
+    static sign(requestInProgress) {
+        requestInProgress.set('Authorization', 'bearer ' + localStorage.getItem('session_token'));
     }
-});
 
-export const logIn = userData => {
-    return request.post(accountsEndpoint)
-        .set("Content-Type", "application/json")
-        .send(userData)
-};
+}
 
-export const sign = (requestInProgress) => requestInProgress.set('Authorization', 'bearer ' + localStorage.getItem('session_token'));
+export default AuthService;
